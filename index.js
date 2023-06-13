3ebc162c21ae86bf3f4d3067c2761acbac5f3832
scrill5158 <mgudeta@outlook.com>
2023-06-13 09:15:09 -0400

    final changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(express.static('build'))
 app.use(cors())
 morgan.token("body", req => JSON.stringify(req.body));
 app.use(
@@ -99,7 +100,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
     })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
