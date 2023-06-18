97f96b55022e9065bf105fa1c709a76a3a3d43c1
scrill5158 <mgudeta@outlook.com>
2023-06-18 11:42:56 -0400

    update

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const generateId = () => {
   app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (body.content === undefined) {
+    if (body.name === undefined) {
       return response.status(400).json({ error: 'content missing' })
     }
   
