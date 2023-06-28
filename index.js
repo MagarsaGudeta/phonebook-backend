6f3911b4afef345aa7b54de7c396f39050146a6e
scrill5158 <mgudeta@outlook.com>
2023-06-28 08:41:33 -0400

    production build

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express()
 const Person = require('./models/person')
 const morgan = require("morgan")
 const cors = require('cors')
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -15,6 +25,8 @@ app.use(
     ":method :url :body - status :status length :res[content-length] - :response-time ms"
   )
 );
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 /* let persons = [
     { 
@@ -49,14 +61,21 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
